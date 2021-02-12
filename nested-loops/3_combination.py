c52b6857b32ec383d5532eb6f32254b02724af93
k1lgor <65338982+k1lgor@users.noreply.github.com>
2021-02-12 19:43:42 +0200

    Delete nested-loops directory

--- nested-loops/3_combination.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-comb = 0
-for i in range(n + 1):
-    for j in range(n + 1):
-        for k in range(n + 1):
-            if i + j + k == n:
-                comb += 1
-print(comb)
--- nested-loops/more-exercises/1_unique_PIN_codes.py
+++
@@ -1,9 +0,0 @@
-x1 = int(input())
-x2 = int(input())
-x3 = int(input())
-
-for i in range(2, x1 + 1, 2):
-    for j in range(2, x2 + 1):
-        for k in range(2, x3 + 1, 2):
-            if j in [2, 3, 5, 7]:
-                print(i, j, k)
